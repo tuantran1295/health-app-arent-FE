7628f2b583c5ad84b34bf21dd85492728577123c
timmy <tuantran1245@gmail.com>
2025-10-24 10:44:38 +0700

    build skeleton layout for the application

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react';
+import { Route, Routes, BrowserRouter } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import Records from './pages/Records';
+import Column from './pages/Column';
+const App: React.FC = () => {
 
-function App() {
-  const [count, setCount] = useState(0)
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<HomePage />} />
+                <Route path="/records" element={<Records />} />
+                <Route path="/column" element={<Column />} />
+            </Routes>
+        </BrowserRouter>
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    );
+};
 
-export default App
+export default App;
