26c42b752a1777ecc08f668384c11c7c7f0d1b15
timmy <tuantran1245@gmail.com>
2025-10-24 15:17:45 +0700

    fix homepage layout

--- src/layout/Header.tsx
+++ src/layout/Header.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import {useState} from 'react';
 import Logo from '../../public/assets/icons/logo.svg';
 import Memo from '../../public/assets/icons/memo.svg';
 import Challenge from '../../public/assets/icons/challenge.svg';
@@ -6,6 +6,7 @@ import Info from '../../public/assets/icons/info.svg';
 import Menu from '../../public/assets/icons/menu.svg';
 import Close from '../../public/assets/icons/close.svg';
 import Dropdown from '../components/Dropdown';
+import {NavLink} from "react-router-dom";
 
 const ELEMENTS = ['自分の記録', '体重グラフ', '目標', '選択中のコース', 'コラム一覧', '設定'];
 
@@ -13,34 +14,55 @@ function Header() {
 
     const [isOpen, setOpen] = useState<boolean>(false);
 
-    return (<div className="h-[84px] w-full bg-dark-600 m-auto">
-            <div className='w-3/4 h-full m-auto flex justify-between'>
-                <a className='flex items-center' href='/'>
-                    <img className='h-[64px] w-[144px]' src={Logo} />
+    return (
+        <div className="h-[84px] w-full bg-dark-600 m-auto">
+            <div className="w-3/4 h-full m-auto flex justify-between">
+                <a className="flex items-center" href="/">
+                    <img className="h-[64px] w-[144px]" src={Logo}/>
                 </a>
-                <div className='flex justify-between items-center w-1/2'>
-                    <div className='inline-flex text-light hover:text-primary-400 cursor-pointer mr-4'>
-                        <img className='h-[32px] w-[32px]' src={Memo} />
-                        <span>自分の記録</span>
+                <div className="flex justify-between items-center w-1/2">
+                    <div className="inline-flex text-light hover:text-primary-400 cursor-pointer mr-4">
+                        <img className="h-[32px] w-[32px]" src={Memo}/>
+                        <NavLink
+                            to="/records"
+                            className={({isActive}) => (isActive ? "text-primary-400" : "")}
+                        >
+                            自分の記録
+                        </NavLink>
                     </div>
-                    <div className='inline-flex text-light hover:text-primary-400 cursor-pointer  mr-4'>
-                        <img className='h-[32px] w-[32px]' src={Challenge} />
+                    <div className="inline-flex text-light hover:text-primary-400 cursor-pointer mr-4">
+                        <img className="h-[32px] w-[32px]" src={Challenge}/>
                         <span>チャレンジ</span>
                     </div>
-                    <div className='inline-flex text-light hover:text-primary-400 cursor-pointer mr-4'>
-                        <img className='h-[32px] w-[32px]' src={Info} />
+                    <div className="inline-flex text-light hover:text-primary-400 cursor-pointer mr-4">
+                        <img className="h-[32px] w-[32px]" src={Info}/>
                         <span>お知らせ</span>
                     </div>
-                    <div className='ml-4'>
+                    <div className="ml-4">
                         <Dropdown open={isOpen} elements={ELEMENTS}>
-                            {
-                                isOpen ? <img onClick={() => { setOpen(false) }} className='h-[32px] w-[32px] cursor-pointer' src={Close} /> : <img onClick={() => { setOpen(true) }} className='h-[32px] w-[32px] cursor-pointer' src={Menu} />
-                            }
+                            {isOpen ? (
+                                <img
+                                    onClick={() => {
+                                        setOpen(false);
+                                    }}
+                                    className="h-[32px] w-[32px] cursor-pointer"
+                                    src={Close}
+                                />
+                            ) : (
+                                <img
+                                    onClick={() => {
+                                        setOpen(true);
+                                    }}
+                                    className="h-[32px] w-[32px] cursor-pointer"
+                                    src={Menu}
+                                />
+                            )}
                         </Dropdown>
                     </div>
                 </div>
             </div>
         </div>
-    )
+    );
 }
+
 export default Header
