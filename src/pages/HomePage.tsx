d3c3de66bac8bf636bea1429029dc54f4b1a82fd
timmy <tuantran1245@gmail.com>
2025-10-24 15:58:38 +0700

    modify some pages layout and add record page

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -11,6 +11,7 @@ import threefour from "/assets/icons/threefour.svg";
 function HomePage() {
     const [expand, setExpand] = useState<boolean>(false);
     const [active, setActive] = useState<string>("all");
+    const [data] = useState({ data1: [7.0, 6.9, 9.5, 14.5, 18.4, 21.5, 25.2, 26.5, 23.3, 18.3, 13.9, 9.6], data2: [3.9, 4.2, 5.7, 8.5, 11.9, 15.2, 17.0, 16.6, 14.2, 10.3, 6.6, 4.8] });
     return (
         <Layout>
             <div>
@@ -24,7 +25,7 @@ function HomePage() {
                         </div>
                     </div>
                     <div className="flex-[60%]">
-                        <Chart/>
+                        <Chart data={data} />
                     </div>
                 </div>
                 <div className="flex mx-auto cursor-pointer items-center w-[700px] justify-between mt-10">
